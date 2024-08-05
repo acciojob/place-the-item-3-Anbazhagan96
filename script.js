9c8322c6ecfd8ebd21f4de91429704b6d18b5d17
Anbazhagan L <128969238+Anbazhagan96@users.noreply.github.com>
2024-08-05 20:52:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+() => {
+    cy.visit(baseUrl + "/main.html");
+    cy.get('.holder').should('have.css', 'display', 'flex');
+    cy.get('.holder').should('have.css', 'justify-content', 'space-between'); // Change to space-between if needed
+    cy.get('.holder').should('have.css', 'flex-wrap', 'wrap');
+}
